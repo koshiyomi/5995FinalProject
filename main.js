8a596bc416b02a09153071642041f18f5a38a7b6
kevin <mashirowhite333@gmail.com>
2019-05-08 14:46:45 -0500

    back

--- main.js
+++ main.js
@@ -2,11 +2,31 @@
  * @author Jianwei Hu, Simeng Qu
  */
 
+const MongoClient = require('mongodb').MongoClient;
+const Server = require('mongodb').Server;
+const url = 'mongodb://localhost:27017/project';
+
+var db;
+var collection;
+
+MongoClient.connect(url, { useNewUrlParser: true },(err, client) => {
+    if (err) {
+        console.error(err);
+        return
+    }
+    console.log("Connection established successfully");
+
+    //...
+    db = client.db("mydb");
+    collection = db.collection("CrimeRecord");
+});
+
+
+
 // TODO: json file for database query
 var queryInfo;
 // TODO: json file for containing result geodata
 var geoData = [{lat: 34.0707, lng: -118.2795},{lat: 34.0737, lng: -118.2795},{lat: 34.0757, lng: -118.2795}];
-
 
 
 /***************************
@@ -30,5 +50,15 @@ app.get("/query",function (request, response) {
 
     //TODO: perform query and renew geodata
 
-    response.send(geoData)
+    var query = {'Area ID': 2};
+
+    collection.aggregate([
+        {$unwind: "$Area Name"},
+        {$sortByCount: "$Area Name"}
+    ]).toArray(function (err, results) {
+        console.log(results)
+        response.send(results);
+    });
 });
+
+
